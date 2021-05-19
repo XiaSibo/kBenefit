5596de292b50889db636cc04b6ab250cb82d70aa
zhangbicheng666 <569107519@qq.com>
2021-05-19 18:24:01 +0800

    上拉刷新下拉加载

--- miniprogram/components/ResponseList/ResponseList.js
+++ miniprogram/components/ResponseList/ResponseList.js
@@ -1,17 +1,17 @@
-// components/PostList/PostList.js
+// components/ResponseList/ResponseList.js
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-      type: String
+
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    postList: [],
+    list: [],  // object: {content, title, post_id}
     page: 1,
     pageSize: 10,
     isRequesting: false, // 是否正在请求数据，防止数据的重复加载
@@ -21,22 +21,10 @@ Component({
 
   lifetimes: {
     attached: function() {
-      console.log(this.data.type)
       this.swipeRefresh = this.selectComponent('#refresh')
       this.swipeRefresh.setRefresh(true)
-    },
-    detached: function() {
-      // 在组件实例被从页面节点树移除时执行
-    },
-  },
-
-  pageLifetimes: {
-    show: function() {
-      console.log("show")
-    },
-    hide: function() {
-      console.log("hide")
-    },
+      this.getData();
+    }
   },
 
   /**
@@ -46,7 +34,7 @@ Component({
     /**
       * 下拉刷新
       */
-     refresh: function() {
+    refresh: function() {
       if (!this.data.isRequesting) {
         this.setData({
           page: 1,
@@ -82,17 +70,17 @@ Component({
           // 请求数据库，传过去skip = (page - 1) * pageSize, limit = pageSize
           // 得到返回值以后直接赋值给list
           this.setData({
-            postList: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             isRequesting: false,
-            hasMore: this.data.postList.length < 15  // hasMore根据数据库返回结果来看
+            hasMore: this.data.list.length < 15  // hasMore根据数据库返回结果来看
           })
         } else {
           // 否则，需要增加一页数据，skip = (page - 1) * pageSize, limit = pageSize
           // 得到返回值以后concat在this.data.list后面
           this.setData({
-            postList: this.data.postList.concat(this.data.postList.length + 1),
+            list: this.data.list.concat(this.data.list.length + 1),
             isRequesting: false,
-            hasMore: this.data.postList.length < 15  // hasMore根据数据库返回结果来看
+            hasMore: this.data.list.length < 15  // hasMore根据数据库返回结果来看
           })
         }
         this.swipeRefresh.setRefresh(false);
